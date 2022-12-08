f648a51a3409ee296158ee62e8010b75cf67f04e
Luong Nguyen <nguyenln@hiworld.com.vn>
2022-12-08 13:11:36 +0700

    update ipfs

--- dms/src/helpers/ipfs.js
+++ dms/src/helpers/ipfs.js
@@ -3,7 +3,7 @@ import { create } from 'ipfs-http-client'
 export const IpfsClient = () => {
     try {
         //const ipfs = window.IpfsHttpClient({ host: '127.0.0.1', port: 5001, protocol: 'http' })
-        const ipfs = window.IpfsHttpClient({ host: '10.10.1.16', port: 7001, protocol: 'http' })
+        const ipfs = window.IpfsHttpClient({ host: '10.10.1.15', port: 7001, protocol: 'http' })
         return ipfs
     } catch (error) {
         console.log("error", error);
